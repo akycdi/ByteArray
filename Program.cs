449c800c590a1f2f9043c24505b547a015cdfc95
Arun <arun36824@gmail.com>
2023-06-30 17:29:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Ports;
+
+namespace ByteArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            SerialPort serialPort = new SerialPort("COM11", 230400);
+            byte[] dataToSend = new byte[16];
+            int[] pwmValues = new int[16];
+            Random random = new Random();
+
+            while (true)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    pwmValues[i] = random.Next(256);
+                    dataToSend[i] = (byte)pwmValues[i];
+                }
+
+                serialPort.Open();
+                serialPort.Write(dataToSend, 0, dataToSend.Length);
+                serialPort.Close();
+
+                Console.WriteLine("Data sent: " + string.Join(", ", pwmValues));
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
