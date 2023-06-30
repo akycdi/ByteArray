ee2ef7a883534789737f33df2463d452f08fb8e0
Arun <arun36824@gmail.com>
2023-06-30 17:40:43 +0530

    WIP with delimiter

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO.Ports;
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
 
 namespace ByteArray
 {
@@ -7,7 +9,7 @@ namespace ByteArray
         static void Main()
         {
             SerialPort serialPort = new SerialPort("COM11", 230400);
-            byte[] dataToSend = new byte[16];
+            byte[] dataToSend = new byte[18];
             int[] pwmValues = new int[16];
             Random random = new Random();
 
@@ -16,15 +18,19 @@ namespace ByteArray
                 for (int i = 0; i < 16; i++)
                 {
                     pwmValues[i] = random.Next(256);
-                    dataToSend[i] = (byte)pwmValues[i];
+                    dataToSend[i + 1] = (byte)pwmValues[i];
                 }
 
-                serialPort.Open();
+                dataToSend[0] = (byte)'{'; // Start delimiter
+                dataToSend[17] = (byte)'}'; // End delimiter
+
+                serialPort.Open(); 
                 serialPort.Write(dataToSend, 0, dataToSend.Length);
-                serialPort.Close();
+                serialPort.Close(); //System crashing so keep the com port close
+
 
                 Console.WriteLine("Data sent: " + string.Join(", ", pwmValues));
-                Thread.Sleep(5000);
+                Thread.Sleep(1); 
             }
         }
     }
